9f5fd82999ebab396abc5a7de5def1b685362486
archanaprabhat <archanaprabhath2002@gmail.com>
2025-07-13 10:50:38 +0530

    mapping

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,19 @@ export default function App() {
   const [currentNote, setCurrentNote] = useState('');
   const [notes, setNotes] = useState([]);
   const handleNote = () => {
-    console.log("Adding note...", currentNote);
+    setNotes([...notes , currentNote])
+    setCurrentNote('')
   };
   return (
     <div>
       <h1>Soch</h1>
       <input type='text' placeholder='Enter your note...' onChange={(e) =>setCurrentNote(e.target.value)} value={currentNote}/>
       <button onClick={handleNote}>Add</button>
+      <div>
+        <ul>
+          notes.map(item, index) => 
+        </ul>
+      </div>
     </div>
   );
 }
