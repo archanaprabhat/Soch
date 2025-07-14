a681f5d4bfaff3224ea815ce555594e55779e696
archanaprabhat <archanaprabhath2002@gmail.com>
2025-07-14 08:49:59 +0530

    add delete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,38 @@
 import { useState } from "react";
 export default function App() {
-  const [currentNote, setCurrentNote] = useState('');
+  const [currentNote, setCurrentNote] = useState("");
   const [notes, setNotes] = useState([]);
   const handleNote = () => {
-    setNotes([...notes , currentNote])
-    setCurrentNote('')
+    setNotes([...notes, currentNote]);
+    setCurrentNote("");
   };
+  const [searchText,setSearchText] = useState('')
+  
+  function handleDelete(id){
+    const newNotes = notes.filter((item, index)=> index !== id)
+    setNotes(newNotes)
+  }
   return (
     <div>
-      <h1>Soch</h1>
-      <input type='text' placeholder='Enter your note...' onChange={(e) =>setCurrentNote(e.target.value)} value={currentNote}/>
+      <div><h1>Soch</h1>
+      <input placeholder='Search for notes here' value={searchText} onChange={(e)=> setSearchText(e.target.value)} type="text" />
+      </div>
+      
+      <input
+        type='text'
+        placeholder='Enter your note...'
+        onChange={(e) => setCurrentNote(e.target.value)}
+        value={currentNote}
+      />
       <button onClick={handleNote}>Add</button>
       <div>
         <ul>
-          notes.map(item, index) => 
+          {notes.map((item, index) => (
+            <li key={index}>
+              {item}
+              <button onClick={() => handleDelete(index)}>Delete</button>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
